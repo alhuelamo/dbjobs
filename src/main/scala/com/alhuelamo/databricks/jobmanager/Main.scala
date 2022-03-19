b8a95c65ad4e97e2f176d19ae2e239df6707b398
alhuelamo <alhuelamo+code@gmail.com>
2022-03-19 21:05:16 +0100

    Cli refactor

--- src/main/scala/com/alhuelamo/databricks/jobmanager/Main.scala
+++ src/main/scala/com/alhuelamo/databricks/jobmanager/Main.scala
@@ -11,11 +11,28 @@ object Main extends CommandApp(
     Cli.opts.mapN { (action, profile, jobIds, plan) =>
       given conf: AppConf = AppConf(action, profile, jobIds, plan)
 
+      import ActionKey.*
       conf.action match {
-        case AppConf.`actionStart` => conf.jobIds.foreach(Actions.startRuns)
-        case AppConf.`actionStop`  => conf.jobIds.foreach(Actions.stopActiveRuns)
-        case invalid => throw new IllegalArgumentException(s"$invalid is not a valid flow (valid ones: start, stop)")
+        case Start => conf.jobIds.foreach(Actions.startRuns)
+        case Stop  => conf.jobIds.foreach(Actions.stopActiveRuns)
       }
     }
   }
 )
+
+object Cli {
+  val opts = (action, profile, jobIds, plan)
+
+  val action: Opts[ActionKey] = Opts.argument[String](metavar = "action")
+    .map(ActionKey.apply)
+
+  val profile: Opts[String] =
+    Opts.option[String]("profile", short = "p", metavar = "profile", help = "Profile section in ~/.databrickscfgProfile section in ~/.databrickscfg")
+
+  val jobIds: Opts[Vector[Long]] = Opts
+    .option[String]("job-ids", short = "s", help = "Comma-separated job IDs to manage")
+    .map(s => s.split(",").map(_.trim.toLong).toVector)
+
+  val plan: Opts[Boolean] = Opts.flag("plan", help = "Enable to just show affected jobs and runs.")
+    .orFalse
+}
--- src/main/scala/com/alhuelamo/databricks/jobmanager/conf/AppConf.scala
+++ src/main/scala/com/alhuelamo/databricks/jobmanager/conf/AppConf.scala
@@ -1,21 +1,35 @@
 package com.alhuelamo.databricks.jobmanager.conf
 
+import cats.syntax.validated
+
 final case class AppConf(
-    action: String,
+    action: ActionKey,
     databricksWs: DatabricksWorkspace,
     jobIds: Vector[Long],
     plan: Boolean = false
 )
 
 object AppConf {
-  val actionStart = "start"
-  val actionStop  = "stop"
-
-  def apply(action: String, profile: String, jobIds: Vector[Long], plan: Boolean): AppConf =
+  def apply(action: ActionKey, profile: String, jobIds: Vector[Long], plan: Boolean): AppConf =
     AppConf(
       action,
       DatabricksWorkspace(profile),
       jobIds,
       plan
     )
+}
+
+enum ActionKey(val value: String) {
+  case Start extends ActionKey("start")
+  case Stop extends ActionKey("stop")
+}
+
+object ActionKey {
+  import ActionKey.*
+
+  def apply(string: String): ActionKey = string match {
+    case Start.`value` => ActionKey.Start
+    case Stop.`value`  => ActionKey.Stop
+    case invalid       => throw new IllegalArgumentException(s"Invalid '$invalid' argument for <action>.")
+  }
 }
